6bcce976dd399fbb47e42f5cf5cc1d9119db2719
Catherine Price <catprice444@gmail.com>
2020-04-22 22:45:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,7 @@ if array.length == 1
 elsif array.length == 2
   return array.join(" and ")
 else array.length >= 3
-  three_or_more = "and #{array[-1]}"
-  array.pop
-  array.push(three_or_more)
+  array[-1].prepend " and "
   return array.join(" , ")
   end
 end
